fb53f7b19b18aaf4bf073dfb752894d55a172d3e
Mohamad Rizal Nurochman <muhamadrizalnurohman072005@gmail.com>
2026-07-13 22:03:14 +0700

    initialize db

--- services/data/internal/database/entities/hub.go
+++ services/data/internal/database/entities/hub.go
@@ -1,8 +1,8 @@
 package entities
 
 type Hub struct {
-	ID       string   `bson:"_id"      json:"id"`     
+	ID       string   `bson:"_id"      json:"id"`
 	Name     string   `bson:"name"     json:"name"`
-	Location GeoPoint `bson:"location" json:"location"` 
-	Capacity int      `bson:"capacity" json:"capacity"` 
+	Location GeoPoint `bson:"location" json:"location"`
+	Capacity int      `bson:"capacity" json:"capacity"`
 }
